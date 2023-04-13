b1963e234125a561e5a4f38a4d91640a31b6f64d
hamletmartirosyan92 <130658701+hamletmartirosyan92@users.noreply.github.com>
2023-04-13 16:11:57 +0400

    Update test_youtube.py

--- test_youtube.py
+++ test_youtube.py
@@ -4,7 +4,7 @@ from playwright.sync_api import Playwright
 def test_youtube(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
-    
+
     host = "https://youtube.com"
     page = browser.new_page()
     page.goto(host)
@@ -19,11 +19,17 @@ def test_youtube(playwright: Playwright) -> None:
     if not page_links.count() >= 10:
         print("there is less then 10 videos about cats")
 
+    links = []
     for link in page_links.all():
         uri = link.get_attribute('href')
         if uri:
             if '/watch' in uri:
-                print(f'{host}{uri}')
+                links.append(f'{host}{uri}')
+
+        if len(links) == 10:
+            break
+
+    print(links)
 
     context.close()
     browser.close()
